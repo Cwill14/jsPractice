59e0bf56787796248518b62a1ecc4e5575d40dc2
Clark Williams <CWillia5@1800contacts.com>
2022-03-02 19:35:24 -0700

    finished wordscore

--- practice2.js
+++ practice2.js
@@ -19,59 +19,44 @@ The returned string should only contain alphabetic characters (a-z).
 Preserve case in the returned string (see 4th example above).
 */
 
+// version 1
+
 function wordRank(str) {
+	// establish letter-point value reference
 	const dict = {
 		"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26
 	}
-	// let arr = str.split("").filter(x => /[a-z])
-	let strArr = str.split(" ")
+	// establish word score storage & starting high score
 	let scores = {}
-	console.log(strArr, "= strArr")
 	let highestScore = 0
-	strArr.map((word, index) => {
-		// console.log(word.filter(c => c !== /^[a-b]/ig.test(c)))
-		let wordArr = word.split("")
-		// let wordArr = word.split("").filter(c => {
-			// console.log(c, "= c")
-			// console.log(/[^a-b]/ig.test(c), "= /[^a-b]/ig.test(c)")
-			// /^[a-z]/ig.test(c)
-			// let x = c.toLowerCase()
-			// console.log(dict.x, "= dict.x")
-			// dict.x == true
-		// })
-		console.log(wordArr, "= wordArr")
-		let currWordScore = 0
-		wordArr.map(char => {
-			// console.log(dict[char.toLowerCase()], "= dict[char.toLowerCase()]")
-			// console.log(char, "= char")
-			// console.log(/[a-z]/ig.test(char), "= /[a-z]/ig.test(char)")
-			if (/[a-z]/ig.test(char)) {
-				currWordScore += dict[char.toLowerCase()]
-			}
+	// separate string into individual words
+	let strArr = str.split(" ")
+	// iterate through each word
+	strArr.map(word => {
+		// separate each word into the letter only characters
+		let wordArr = word.split("").filter(c => {
+			// regex that returns true if letter only (case insensitive) character
+			return /[a-z]/ig.test(c)
 		})
-		console.log(word, "= word")
-		console.log(currWordScore, "= currWordScore")
-		scores[currWordScore] = word
-		currWordScore > highestScore
-			? highestScore = currWordScore
-			: console.log("nope")
-		// console.log(scores, "= scores")
-		// console.log(x, "= x")
-		// console.log(/[a-z]/i.test(x))
-		// // console.log(dict[x.toLowerCase()], "= dict[x.toLowerCase()]")
-		// // console.log("------")
-		// if (x == /[a-z]/i.test()) {
-		// 	console.log(dict[x], "= dict[x]")
-		// 	sum += dict[x.toLowerCase()]
-		// } else {
-		// 	console.log("next pls")
-		// }
+		// store the word now w/o non letter chars so that final return is only the word
+		let correctWord = wordArr.join("")
+		// create running counter for total score for the word
+		let currWordScore = 0
+		// iterate through characters
+		wordArr.map(char => {
+			// add the value of selected character to running total
+			currWordScore += dict[char.toLowerCase()]
+		})
+		// once word total calculated, store that word score & word in storage for reference oustide map
+		scores[currWordScore] = correctWord
+		// if the word total score is new highest score, set new high score
+		if (currWordScore > highestScore) highestScore = currWordScore
 	})
-	// console.log(highestScore, "= highestScore")
-	// console.log(scores[highestScore])
+	// once all iterations complete, return the word from storage that is associated with the highest score
 	return scores[highestScore]
 }
 
-// console.log(wordRank("Quick brown fox"))
-// console.log(wordRank("Nancy is very pretty"))
-console.log(wordRank("Check back tomorrow, man"))
+console.log(wordRank("Quick brown fox")) // brown
+console.log(wordRank("Nancy is very pretty")) // pretty
+console.log(wordRank("Check back tomorrow, man")) // tomorrow
+console.log(wordRank("Wednesday is hump day.")) // Wednesday
