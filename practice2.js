d00f3054b4b5d0b13658d327596cdf8cf8f020fd
Clark Williams <CWillia5@1800contacts.com>
2022-03-22 19:23:34 -0600

    Crack the Code

--- practice2.js
+++ practice2.js
@@ -477,7 +477,9 @@ console.log("---------------------------------------------")
 console.log(getHashTags("Visualizing Science"))
 // ["#visualizing", "#science"]
 
+
 //														3/22/22
+
 
 /*									Nearest Vowel
 Given a letter, created a function which returns the nearest vowel to the letter. If two vowels are equidistant to the given letter, return the earlier vowel.
@@ -547,3 +549,52 @@ console.log(nearestVowel("f"), "<- should be e") // "e"
 console.log("==================")
 console.log(nearestVowel("r"), "<- should be o") // "o"
 
+				
+/*
+												Crack the Code
+This is a reverse-coding challenge. Create a function that outputs the correct array from the input. Use the following examples to crack the code.
+
+Examples
+decode("hello") ➞ [5, 2, 9, 9, 3]
+
+decode("wonderful") ➞ [11, 3, 2, 1, 2, 6, 3, 9, 9]
+
+decode("something challenging") ➞ [7, 3, 10, 2, 8, 5, 6, 2, 4, 5, 18, 5, 16, 9, 9, 2, 2, 4, 6, 2, 4]
+*/
+
+function decode(str) {
+	const code = {
+		"h": 5,
+		"e": 2,
+		"l": 9,
+		"o": 3,
+		"w": 11,
+		"n": 2,
+		"d": 1,
+		"r": 6,
+		"f": 3,
+		"u": 9,
+		"s": 7,
+		"m": 10,
+		"t": 8,
+		"i": 6,
+		"g": 4,
+		" ": 5,
+		"c": 18,
+		"a": 16
+	}
+	let nArr = []
+	str.split("").map(c => {
+		nArr.push(code[c])
+		// console.log("c =", c)
+	})
+	return nArr
+}
+console.log("hello")
+console.log(decode("hello"), "<- should be [5, 2, 9, 9, 3]") // [5, 2, 9, 9, 3]
+console.log("===================")
+console.log("wonderful")
+console.log(decode("wonderful"), "<- should be [11, 3, 2, 1, 2, 6, 3, 9, 9]") // [11, 3, 2, 1, 2, 6, 3, 9, 9]
+console.log("===================")
+console.log("something challenging")
+console.log(decode("something challenging"), "<- should be [7, 3, 10, 2, 8, 5, 6, 2, 4, 5, 18, 5, 16, 9, 9, 2, 2, 4, 6, 2, 4]") // [7, 3, 10, 2, 8, 5, 6, 2, 4, 5, 18, 5, 16, 9, 9, 2, 2, 4, 6, 2, 4]
