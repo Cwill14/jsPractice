cd7b4300ceedc4dbdb77225c009d5809989a6018
Clark Williams <CWillia5@1800contacts.com>
2022-03-22 18:52:34 -0600

    Nearest Vowel

--- practice2.js
+++ practice2.js
@@ -476,3 +476,74 @@ console.log(getHashTags("Hey Parents, Surprise, Fruit Juice Is Not Fruit"))
 console.log("---------------------------------------------")
 console.log(getHashTags("Visualizing Science"))
 // ["#visualizing", "#science"]
+
+//														3/22/22
+
+/*									Nearest Vowel
+Given a letter, created a function which returns the nearest vowel to the letter. If two vowels are equidistant to the given letter, return the earlier vowel.
+
+Examples
+nearestVowel("b") ➞ "a"
+
+nearestVowel("s") ➞ "u"
+
+nearestVowel("c") ➞ "a"
+
+nearestVowel("i") ➞ "i"
+Notes
+All letters will be given in lowercase.
+There will be no alphabet wrapping involved, meaning the closest vowel to "z" should return "u", not "a".
+*/
+
+function nearestVowel(s) {
+	const vowels = ["a", "e", "i", "o", "u"]
+	const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+	if (vowels.includes(s)) return s
+	else {
+		let fCount = 0;
+		let bCount = 0;
+		let sIndex = alphabet.indexOf(s)
+		let fVowel = "0"
+		let bVowel = "0"
+		console.log(s, sIndex, "start here")
+		console.log("-------")
+		// forwards loop
+		for (let i = sIndex + 1; i < alphabet.length; i++) {
+			if (vowels.includes(alphabet[i])) {
+				fVowel = alphabet[i]
+				break
+			}
+			fCount++
+			console.log("f =", alphabet[i], i, fCount)
+		}
+		// backwards loop
+		for (let i = sIndex -1; i > -1; i--) {
+			if (vowels.includes(alphabet[i])) {
+				bVowel = alphabet[i]
+				break
+			}
+			bCount++
+			console.log("b =", alphabet[i], i, bCount)
+		}
+		console.log("fCount =", fCount)
+		console.log("bCount =", bCount)
+		console.log("fVowel =", fVowel)
+		console.log("bVowel =", bVowel)
+		console.log("-------")
+		return fCount >= bCount
+			? bVowel
+			:	fVowel 
+	}
+}
+console.log(nearestVowel("b"), "<- should be a") // "a"
+console.log("==================")
+console.log(nearestVowel("s"), "<- should be u") // "u"
+console.log("==================")
+console.log(nearestVowel("c"), "<- should be a") // "a"
+console.log("==================")
+console.log(nearestVowel("i"), "<- should be i") // "i"
+console.log("==================")
+console.log(nearestVowel("f"), "<- should be e") // "e"
+console.log("==================")
+console.log(nearestVowel("r"), "<- should be o") // "o"
+
