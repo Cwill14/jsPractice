c2a3b2cc3ca91cf662fa0f16164f0c6e6849bc1d
Clark Williams <CWillia5@1800contacts.com>
2022-05-10 19:43:44 -0600

    valid sets

--- practice4.js
+++ practice4.js
@@ -417,3 +417,119 @@ console.log("------------------")
 	gender: "M",
 	dob: "14/6/1995"
   }), "WLLCRK95H14")
+
+// 5/10/22
+
+//  Validating a Set in the Set Game
+/*
+In the game Set, three cards form a set if each of the cards are identical or completely different for each of the four properties. All three cards must:
+
+Have the same color or different colors.
+Have the same number or different numbers.
+Have the same shades or different shades.
+Have the same shape or different shapes.
+The four properties are:
+
+Colors: red, purple, green
+Numbers: 1, 2, 3
+Shades: empty, lined, full
+Shapes: squiggle, oval, diamond
+Here, a set is represented by an array containing three cards. Each card is represented by an object of properties and values. Write a function that determines whether three cards constitute a valid set.
+
+Here is an example of a set:
+
+[
+  { color: "red", number: 1, shade: "empty", shape: "squiggle" },
+  { color: "red", number: 2, shade: "lined", shape: "diamond" },
+  { color: "red", number: 3, shade: "full", shape: "oval" }
+]
+
+// "Same" properties: color
+// "Different" properties: numbers, shading and shapes
+The following is not a set:
+
+[
+  { color: "red", number: 1, shade: "empty", shape: "squiggle" },
+  { color: "red", number: 2, shade: "lined", shape: "diamond" },
+  { color: "purple", number: 3, shade: "full", shape: "oval" }
+]
+
+// Colors are not all identical, but not all different.
+Examples
+isSet([
+  { color: "green", number: 1, shade: "empty", shape: "squiggle" },
+  { color: "green", number: 2, shade: "empty", shape: "diamond" },
+  { color: "green", number: 3, shade: "empty", shape: "oval" }
+]) ➞ true
+
+isSet([
+  { color: "purple", number: 1, shade: "full", shape: "oval" },
+  { color: "green", number: 1, shade: "full", shape: "oval" },
+  { color: "red", number: 1, shade: "full", shape: "oval" }
+]) ➞ true
+
+isSet([
+  { color: "purple", number: 3, shade: "full", shape: "oval" },
+  { color: "green", number: 1, shade: "full", shape: "oval" },
+  { color: "red", number: 3, shade: "full", shape: "oval" }
+]) ➞ false
+Notes
+A set cannot have 2/3 cards having the same property. Either all must share that property, or none will share that particular property.
+You can play Set by checking the Resources tab.
+*/
+
+function isSet(cards) {
+	let ans = true
+	// default return is true
+	// const col = cards[0].color
+	// let [col, num, shad, shap] = Array(4).fill(new Set)
+	let col = new Set
+	let num = new Set
+	let shad = new Set
+	let shap = new Set
+	let sets = [col, num, shad, shap]
+	
+	cards.forEach(card => {
+		col.add(card.color)
+		num.add(card.number)
+		shad.add(card.shade)
+		shap.add(card.shape)
+	})
+
+	sets.forEach(set => {
+		// if (x.size !== 3 && x.size !== 1) {
+		if (set.size == 2) ans = false
+	})
+
+	return ans
+}
+
+console.log(isSet([
+  { color: "green", number: 1, shade: "empty", shape: "squiggle" },
+  { color: "green", number: 2, shade: "empty", shape: "diamond" },
+  { color: "green", number: 3, shade: "empty", shape: "oval" }
+]), "true") // true
+
+console.log(isSet([
+  { color: "purple", number: 1, shade: "full", shape: "oval" },
+  { color: "green", number: 1, shade: "full", shape: "oval" },
+  { color: "red", number: 1, shade: "full", shape: "oval" }
+]), "true") // true
+
+console.log(isSet([
+  { color: "purple", number: 3, shade: "full", shape: "oval" },
+  { color: "green", number: 1, shade: "full", shape: "oval" },
+  { color: "red", number: 3, shade: "full", shape: "oval" }
+]), "false") // false
+
+console.log(isSet([
+  { color: "red", number: 1, shade: "empty", shape: "squiggle" },
+  { color: "red", number: 2, shade: "lined", shape: "diamond" },
+  { color: "purple", number: 3, shade: "full", shape: "oval" }
+]), "false") // false
+
+console.log(isSet([
+  { color: "red", number: 1, shade: "empty", shape: "squiggle" },
+  { color: "red", number: 2, shade: "lined", shape: "diamond" },
+  { color: "red", number: 3, shade: "full", shape: "oval" }
+]), "true") //true
