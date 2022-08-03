5a2ca444d508b66ae0f29f554b2755a57fb9ad1a
Clark Williams <48037077+Cwill14@users.noreply.github.com>
2022-08-02 19:40:01 -0600

    weeklySalary

--- practice5.js
+++ practice5.js
@@ -286,3 +286,54 @@ console.log(getValue('[[[[[[[[[[[]]]]]]]]]]]'), "What... why did you make this?"
 console.log("----------------------------------------------")
 console.log(getValue(`[[][][][][][][][[][][[[[[[["I have no idea what i'm doing"]]]]]]]]]`), 
 "I have no idea what i'm doing")
+
+//										8/2/22
+
+/*
+Weekly Salary
+Write a function that takes a list of hours and returns the total weekly salary.
+
+The input list hours is listed sequentially, ordered from Monday to Sunday.
+A worker earns $10 an hour for the first 8 hours.
+For every overtime hour, he earns $15.
+On weekends, the employer pays double the usual rate, regardless how many hours were worked previously that week. For instance, 10 hours worked on a weekday would pay 80+30 = $110, but on a weekend it would pay 160+60 = $220.
+Examples
+weeklySalary([8, 8, 8, 8, 8, 0, 0]) ➞ 400
+
+weeklySalary([10, 10, 10, 0, 8, 0, 0]) ➞ 410
+
+weeklySalary([0, 0, 0, 0, 0, 12, 0]) ➞ 280
+Notes
+Every element in the array is greater than or equal to 0.
+*/
+
+function weeklySalary(hours) {
+	// create running total
+	let salary = 0;
+	// loop through array
+	for (let i = 0; i < hours.length; i++) {
+		// create current day total
+		let current = 0;
+		// if 8 or less hours, add that * 10 to current total
+		if (hours[i] <= 8) {
+			current += hours[i] * 10;
+		} else {
+		// if more than 8, add $80, add x-8 * 15
+			let x = hours[i] - 8;
+			current += (x * 15) + 80
+		}
+		// if last two indexes in arr (saturday & sunday), double current total
+		if (i == 5 || i == 6) {
+			current *= 2
+		}
+		// add current total to weekly total
+		salary += current
+	}
+	return salary
+}
+
+console.log(weeklySalary([8, 8, 8, 8, 8, 0, 0]), " should be 400") // 400
+
+console.log(weeklySalary([10, 10, 10, 0, 8, 0, 0]), " should be 410") // 410
+
+console.log(weeklySalary([0, 0, 0, 0, 0, 12, 0]), " should be 280") // 280
